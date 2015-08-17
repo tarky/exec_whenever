57325197a669ab5f170b5f66e52ef2efff646f23
tarky <taisuke.araki@gmail.com>
2015-08-17 17:47:52 +0900

    set path and use -i instead of update-crontab.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,8 @@
 app_name = node[:opsworks][:applications][0][:name]
 rails_env = node[:deploy][app_name][:rails_env]
+path = "/srv/www/#{app_name}/current"
 
 execute 'whenever' do
-  cwd "/srv/www/#{app_name}/current"
-  command "bundle exec whenever --update-cron #{app_name}_#{rails_env} -f config/schedule_check.rb"
+  cwd path
+  command "bundle exec whenever -i #{app_name}_#{rails_env} -s \"path=#{path}\" -f config/schedule_check.rb"
 end
